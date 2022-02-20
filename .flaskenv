332d483d34eace0cf24bf26ad20a0f0afddf4237
Lukasz Zytko <zytko.lukasz@gmail.com>
2022-02-20 21:45:23 +0100

    Init project

---
+++ .flaskenv
@@ -0,0 +1,2 @@
+FLASK_APP=jukebox.py
+FLASK_ENV=development
---
+++ app/routes.py
@@ -0,0 +1,7 @@
+from flask import render_template
+from app import app
+from flask import render_template
+
+@app.route("/")
+def home():
+    return render_template("home.html")
